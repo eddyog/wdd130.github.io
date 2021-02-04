5d007ddaab2c550f2560da6e2479a9d27c3194c6
eddyog <gon19005@byui.edu>
2021-02-03 17:50:14 -0700

    update

--- lesson-4-Preston-Town-Page/java.js
+++ lesson-4-Preston-Town-Page/java.js
@@ -12,3 +12,10 @@ window.addEventListener('load', (event)=>{
     hambutton.addEventListener('click', ()=> {mainnav.classList.toggle('responsive')}, false);
 })
 
+window.addEventListener('load', (event)=>{
+    const lu = document.querySelector ('#lastupdated');
+    lu.textContent = document.lastModified;
+
+    const cry = document.querySelector("#copyrightyear");
+    cry.textContent = new Date().getFullYear();
+})
