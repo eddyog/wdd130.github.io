d58a09720a8cf463f043af318a1baa9c242c724e
eddyog <gon19005@byui.edu>
2021-02-03 17:20:59 -0700

    update

---
+++ WO5 Preston Town Page/java.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', (event)=>{
+    const lu = document.querySelector ('#lastupdated');
+    lu.textContent = document.lastModified;
+
+    const cry = document.querySelector("#copyrightyear");
+    cry.textContent = new Date().getFullYear();
+
+
+    const hambutton = document.querySelector('.ham');
+    const mainnav  = document.querySelector('#navigation');
+    
+    hambutton.addEventListener('click', ()=> {mainnav.classList.toggle('responsive')}, false);
+})
+
+window.addEventListener('load', (event)=>{
+    const lu = document.querySelector ('#lastupdated');
+    lu.textContent = document.lastModified;
+
+    const cry = document.querySelector("#copyrightyear");
+    cry.textContent = new Date().getFullYear();
+})
