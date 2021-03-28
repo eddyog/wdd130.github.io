942ad923f1e371072ddcde7ac7cc6798a96486ec
eddyog <gon19005@byui.edu>
2021-03-28 07:46:38 -0600

    update

--- weather_site/soda_springs/wind_chill.js
+++ weather_site/soda_springs/wind_chill.js
@@ -1,54 +1,17 @@
 
-
-window.addEventListener('load', (event)=>{
-    const lu = document.querySelector ('#lastupdated');
-    lu.textContent = document.lastModified;
-
-    const cry = document.querySelector("#copyrightyear");
-    cry.textContent = new Date().getFullYear();
-
-
-    const hambutton = document.querySelector('.ham');
-    const mainnav  = document.querySelector('#navigation');
+window.addEventListener('load', () => {
+    let temp = document.getElementById("temp").innerHTML;
+    let wind = document.getElementById("speed").innerHTML;
     
-    hambutton.addEventListener('click', ()=> {mainnav.classList.toggle('responsive')}, false);
-})
-
-window.addEventListener('load', (event)=>{
-    const lu = document.querySelector ('#lastupdated');
-    lu.textContent = document.lastModified;
-
-    const cry = document.querySelector("#copyrightyear");
-    cry.textContent = new Date().getFullYear();
-})
-
-
-window.addEventListener('load', (event)=>{ 
-
-    
-    var myDate = new Date();
-    var hrs = myDate.getHours();
-    // Get DayOfWeek from int value
-    var greet;
-    if (hrs < 12)
-        greet = 'Good Morning ';
-    else if (hrs >= 12 && hrs <= 17)
-        greet = 'Good Afternoon ';
-    else if (hrs >= 17 && hrs <= 24)
-        greet = 'Good Evening ';
-
-        
-
-    document.getElementById('lblGreetings').innerHTML =
-        '<b>' + greet + '\n and welcome to The Hottie Weather!';
-})
-
-window.addEventListener('load', (event)=> {
-    var d = new Date();
-    var n = d.getDay();
-    var greet = '';
-
-    if(n == 5)
-        greet = 'Friday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.'
-    document.getElementById("demo").innerHTML = greet;
-})
+ if (temp <= 50 && wind > 3) {
+    let chill = 35.74 + (0.6215 * temp) - (35.75 * (wind ** 0.16)) + (0.4275 * (temp * (wind ** 0.16)));
+    console.log(chill);
+    document.getElementById("chill").innerHTML = `${Math.round( chill * 100 )/100} *F`;
+     }
+ else {	
+     console.log("N/A");
+     document.getElementById("chill").innerHTML = ("N/A");
+ }
+ 
+ });
+ 
