e74a9cbcbbeb9da7839c90d35df0f23994f8c293
Eddy Gonzalez <gon19005@byui.edu>
2022-02-10 14:48:27 -0700

    update

--- weather_site/fish_heaven/java.js
+++ weather_site/fish_heaven/java.js
@@ -1,11 +1,6 @@
 
 
 window.addEventListener('load', (event)=>{
-    const lu = document.querySelector ('#lastupdated');
-    lu.textContent = document.lastModified;
-
-    const cry = document.querySelector("#copyrightyear");
-    cry.textContent = new Date().getFullYear();
 
 
     const hambutton = document.querySelector('.ham');
